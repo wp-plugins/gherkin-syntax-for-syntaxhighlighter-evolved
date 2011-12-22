c9ecf48765978e908378adbd74e92cc6b3d7abca
michaelhall <michaelhall@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-22 20:55:00 +0000

    Turns out the MS Exchange server ate the JS file...
    git-svn-id: https://plugins.svn.wordpress.org/gherkin-syntax-for-syntaxhighlighter-evolved/trunk@479515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shBrushGherkin.js
+++ shBrushGherkin.js
@@ -1,5 +1,1 @@
-FILE QUARANTINED
-
-Microsoft Forefront Protection for Exchange Server removed a file since it was found to match a filter.
-File name: "winmail.dat->gherkin-highlighter.zip->shBrushGherkin.js"
-Filter name: "FILE FILTER= Default Filter List: *.js"
+/* Exchange server ate me */
